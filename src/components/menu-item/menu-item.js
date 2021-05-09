119969edd095fd84cecff6168a1875b65eca7e75
Gokul Kannan <gokulkannan@Gokuls-MacBook-Air.local>
2021-05-09 15:29:42 +0530

    broken our homepage nto smaller component, finished styling our menu-item component

---
+++ src/components/menu-item/menu-item.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './menu-item.scss';
+
+const MenuItem = ({ title, imageURL, size }) => (
+    <div 
+        className={`${size} menu-item`}>
+        <div
+            className='background-image'
+            style={{
+                backgroundImage: `url(${imageURL})`
+            }}
+        />
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+    </div>
+)
+
+export default MenuItem
